b8971b6eb8fefdff5680cc7086f103230d093953
bsh010908 <bsh019098@gmail.com>
2025-09-09 00:15:57 +0900

    x

--- public/assets/js/logomodule.js
+++ public/assets/js/logomodule.js
@@ -510,7 +510,7 @@ const tabsData = [
     { href: "calendar", icon: "fas fa-calendar-alt", label: "개봉날짜" },
     { href: "stati", icon: "fas fa-chart-bar", label: "영화평점" },
     { href: "config", icon: "fas fa-tools", label: "통계" },
-    { href: "coupon", icon: "fas fa-briefcase", label: "리" },
+    { href: "coupon", icon: "fas fa-briefcase", label: "미니게임" },
 ];
 
 function renderTabs(containerId) {
